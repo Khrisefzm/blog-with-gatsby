6c7db2e6a4ce2239726026a0f2c9b2e1e5338b3a
Khrisefzm <khrisefzm@gmail.com>
2024-01-24 19:02:06 -0500

    in middle of part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "My blog with Gatsby",
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
